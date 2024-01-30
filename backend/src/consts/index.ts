44aa7ba81cfec37705eb753ae89c748fbfd51a02
freefly0125 <simonbergman516@gmail.com>
2024-01-30 14:45:51 +0000

    feat:define types

---
+++ backend/src/consts/index.ts
@@ -0,0 +1,1 @@
+export * as TYPE from './types';
---
+++ backend/src/consts/types.ts
@@ -0,0 +1,10 @@
+export interface IUserData {
+  name: string;
+  token: number;
+}
+
+export interface ITransactionData {
+  sender: string;
+  receiver: string;
+  amount: number;
+}
